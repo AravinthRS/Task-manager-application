7c39bcc951194b0e087b37d903ad21b4eda9e991
Aravinth R <aravinthrs1705@gmail.com>
2023-05-20 21:13:18 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Pages/Login";
+import UserPage from "./Pages/UserPage";
+import Dashboard, { loader as homePageLoader } from "./Pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: homePageLoader,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    index: true,
+  },
+  {
+    path: "/user/:id",
+    element: <UserPage />,
+    index: true,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
